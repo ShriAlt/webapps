e0ba04561d59be0770eee29e54e18e333a47e91b
Shriharsha K M <shriharshakm10@gmail.com>
2025-05-03 20:12:35 +0530

    checking connection

--- fire/src/WaterServlet.java
+++ fire/src/WaterServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
-@WebServlet(urlPatterns = "/water",loadOnStartup = 0)
+@WebServlet(urlPatterns = "/water",loadOnStartup = 1)
 public class WaterServlet extends GenericServlet {
     public  WaterServlet(){
         System.out.println("instance created fire tomcat");
@@ -15,8 +15,11 @@ public class WaterServlet extends GenericServlet {
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         System.out.println("running method water tomcat");
-
         String name=servletRequest.getParameter("name");
+        String location=servletRequest.getParameter("location");
+        String quantity=servletRequest.getParameter("quality");
         System.out.println(name);
+        System.out.println(location);
+        System.out.println(quantity);
     }
 }
