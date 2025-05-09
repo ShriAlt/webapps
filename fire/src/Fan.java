e234834d3e767da47f4600a33fcbb8cec2de3257
Shriharsha K M <shriharshakm10@gmail.com>
2025-05-09 23:47:11 +0530

    checking connection

---
+++ fire/src/Fan.java
@@ -0,0 +1,19 @@
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/fan" , loadOnStartup = 1)
+public class Fan extends GenericServlet {
+    public Fan(){
+        System.out.println("running the servlet by tom cat");
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+        System.out.println("running the fan Servlet");
+    }
+}
